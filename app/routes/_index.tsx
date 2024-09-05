e39936a4e31c098282be2f6995f6a6367d7d385b
kkosuge <root@kksg.net>
2024-09-05 23:16:41 +0900

    biome

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,123 +9,123 @@ const defaultText = "大きな文字でXに\nポストしてみませんか";
 const baseURL = "https://xtaro.kksg.net";
 
 export const meta: MetaFunction = (context) => {
-	const search = context.location.search;
-	const t = new URLSearchParams(search).get("t") || defaultText;
+  const search = context.location.search;
+  const t = new URLSearchParams(search).get("t") || defaultText;
 
-	return [
-		{ title: "大きな文字でXにポストしてみませんか" },
-		{ name: "description", content: "大きな文字でXにポストしてみませんか" },
-		{
-			"og:image": `${baseURL}/ogp?t=${encodeURIComponent(t)}`,
-		},
-	];
+  return [
+    { title: "大きな文字でXにポストしてみませんか" },
+    { name: "description", content: "大きな文字でXにポストしてみませんか" },
+    {
+      "og:image": `${baseURL}/ogp?t=${encodeURIComponent(t)}`,
+    },
+  ];
 };
 
 type LoaderData = {
-	t: string;
+  t: string;
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
-	const search = new URL(request.url).searchParams;
-	const t = search.get("t") || defaultText;
+  const search = new URL(request.url).searchParams;
+  const t = search.get("t") || defaultText;
 
-	return { t } satisfies LoaderData;
+  return { t } satisfies LoaderData;
 };
 
 export default function Index() {
-	const { t } = useLoaderData<LoaderData>();
-	const [w] = useWindowSize();
-	const [text, setText] = useState(t);
+  const { t } = useLoaderData<LoaderData>();
+  const [w] = useWindowSize();
+  const [text, setText] = useState(t);
 
-	let cardWidth = w - 16 * 4 - 2;
-	if (cardWidth > 360) {
-		cardWidth = 360;
-	}
-	const cardHeight = (630 / 1200) * cardWidth;
-	const fontSize = 36 * (cardWidth / 460);
+  let cardWidth = w - 16 * 4 - 2;
+  if (cardWidth > 360) {
+    cardWidth = 360;
+  }
+  const cardHeight = (630 / 1200) * cardWidth;
+  const fontSize = 36 * (cardWidth / 460);
 
-	const lines = text.split("\n");
+  const lines = text.split("\n");
 
-	const blocks = lines.map((line, index) => {
-		let paddingTop = cardHeight / 2 - (fontSize / 2 + 4) * lines.length;
+  const blocks = lines.map((line, index) => {
+    let paddingTop = cardHeight / 2 - (fontSize / 2 + 4) * lines.length;
 
-		if (index > 0 && lines[index - 1].length > 10) {
-			paddingTop = paddingTop + fontSize;
-		}
+    if (index > 0 && lines[index - 1].length > 10) {
+      paddingTop = paddingTop + fontSize;
+    }
 
-		return (
-			<div
-				key={line}
-				style={{
-					position: "absolute",
-					top: `${paddingTop + index * (fontSize + 4)}px`,
-					lineHeight: `${fontSize}px`,
-					padding: "0 4px",
-				}}
-			>
-				{line}
-			</div>
-		);
-	});
+    return (
+      <div
+        key={line}
+        style={{
+          position: "absolute",
+          top: `${paddingTop + index * (fontSize + 4)}px`,
+          lineHeight: `${fontSize}px`,
+          padding: "0 4px",
+        }}
+      >
+        {line}
+      </div>
+    );
+  });
 
-	const intentUrl = `https://twitter.com/intent/tweet?hashtags=${encodeURIComponent("X太郎")}&text=${encodeURI(`${baseURL}?t=${encodeURIComponent(text)}`)}`;
+  const intentUrl = `https://twitter.com/intent/tweet?hashtags=${encodeURIComponent("X太郎")}&text=${encodeURI(`${baseURL}?t=${encodeURIComponent(text)}`)}`;
 
-	return (
-		<div className="font-sans pt-16 pb-0 h-screen">
-			<div className="flex flex-col gap-4 h-full">
-				<div className="flex flex-col gap-4 border rounded p-4 max-w-[426px] mx-auto">
-					<div className="flex gap-2">
-						<img
-							src="/icon.webp"
-							alt="User Icon"
-							className="w-[50px] h-[50px] rounded-full"
-						/>
-						<div className="flex flex-col">
-							<div className="flex gap-1">
-								<h3 className="font-bold">X太郎</h3>
-								<div className="flex items-center align-center">
-									<img src="/blue.webp" alt="blue mark" className="w-4 h-4" />
-								</div>
-							</div>
-							<div className="text-gray-500">@xxxxxxtaro</div>
-						</div>
-					</div>
-					<div
-						className="border rounded p-8 relative font-semibold"
-						style={{
-							width: `${cardWidth}px`,
-							height: `${cardHeight}px`,
-							fontSize: `${fontSize}px`,
-						}}
-					>
-						{blocks}
-					</div>
-				</div>
+  return (
+    <div className="font-sans pt-16 pb-0 h-screen">
+      <div className="flex flex-col gap-4 h-full">
+        <div className="flex flex-col gap-4 border rounded p-4 max-w-[426px] mx-auto">
+          <div className="flex gap-2">
+            <img
+              src="/icon.webp"
+              alt="User Icon"
+              className="w-[50px] h-[50px] rounded-full"
+            />
+            <div className="flex flex-col">
+              <div className="flex gap-1">
+                <h3 className="font-bold">X太郎</h3>
+                <div className="flex items-center align-center">
+                  <img src="/blue.webp" alt="blue mark" className="w-4 h-4" />
+                </div>
+              </div>
+              <div className="text-gray-500">@xxxxxxtaro</div>
+            </div>
+          </div>
+          <div
+            className="border rounded p-8 relative font-semibold"
+            style={{
+              width: `${cardWidth}px`,
+              height: `${cardHeight}px`,
+              fontSize: `${fontSize}px`,
+            }}
+          >
+            {blocks}
+          </div>
+        </div>
 
-				<a
-					href={intentUrl}
-					target="_blank"
-					rel="noreferrer"
-					className="bg-black text-white font-bold text-xl w-full block rounded text-center py-2 hover:bg-gray-800 max-w-[394px] mx-auto"
-				>
-					𝕏 にポストする
-				</a>
+        <a
+          href={intentUrl}
+          target="_blank"
+          rel="noreferrer"
+          className="bg-black text-white font-bold text-xl w-full block rounded text-center py-2 hover:bg-gray-800 max-w-[394px] mx-auto"
+        >
+          𝕏 にポストする
+        </a>
 
-				<TextareaAutosize
-					className="w-full h-[100px] border rounded px-6 py-4  resize-none bg-gray-100 text-xl text-gray-600 max-w-[394px] mx-auto"
-					value={text}
-					onChange={(e) => setText(e.target.value)}
-				/>
+        <TextareaAutosize
+          className="w-full h-[100px] border rounded px-6 py-4  resize-none bg-gray-100 text-xl text-gray-600 max-w-[394px] mx-auto"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
 
-				<a
-					href="https://x.com/9m"
-					target="_blank"
-					className="text-center mt-auto bg-black text-white text-xs hover:bg-gray-800 py-2"
-					rel="noreferrer"
-				>
-					𝕏 @9m
-				</a>
-			</div>
-		</div>
-	);
+        <a
+          href="https://x.com/9m"
+          target="_blank"
+          className="text-center mt-auto bg-black text-white text-xs hover:bg-gray-800 py-2"
+          rel="noreferrer"
+        >
+          𝕏 @9m
+        </a>
+      </div>
+    </div>
+  );
 }
--- functions/ogp.tsx
+++ functions/ogp.tsx
@@ -1,52 +1,52 @@
 import { ImageResponse } from "@cloudflare/pages-plugin-vercel-og/api";
 
 export const onRequest: PagesFunction = async (context) => {
-	const url = context.request.url;
-	const search = new URL(url).searchParams;
-	const t = search.get("t") || "大きな文字でXに\nポストしてみませんか";
+  const url = context.request.url;
+  const search = new URL(url).searchParams;
+  const t = search.get("t") || "大きな文字でXに\nポストしてみませんか";
 
-	const lines = t?.split("\n");
+  const lines = t?.split("\n");
 
-	const blocks = lines.map((line, index) => {
-		let paddingTop = 630 / 2 - 64 * lines.length;
+  const blocks = lines.map((line, index) => {
+    let paddingTop = 630 / 2 - 64 * lines.length;
 
-		if (index > 0 && lines[index - 1].length > 10) {
-			paddingTop = paddingTop + 120;
-		}
+    if (index > 0 && lines[index - 1].length > 10) {
+      paddingTop = paddingTop + 120;
+    }
 
-		return (
-			<div
-				key={line}
-				style={{
-					position: "absolute",
-					top: `${paddingTop + index * 120}px`,
-					lineHeight: "100px",
-					padding: "0 100px",
-				}}
-			>
-				{line}
-			</div>
-		);
-	});
+    return (
+      <div
+        key={line}
+        style={{
+          position: "absolute",
+          top: `${paddingTop + index * 120}px`,
+          lineHeight: "100px",
+          padding: "0 100px",
+        }}
+      >
+        {line}
+      </div>
+    );
+  });
 
-	return new ImageResponse(
-		<div
-			style={{
-				display: "flex",
-				flexDirection: "column",
-				fontSize: "100px",
-				gap: "10px",
-				background: "#FFF",
-				width: "100%",
-				height: "100%",
-				position: "relative",
-			}}
-		>
-			{blocks}
-		</div>,
-		{
-			width: 1200,
-			height: 630,
-		},
-	);
+  return new ImageResponse(
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        fontSize: "100px",
+        gap: "10px",
+        background: "#FFF",
+        width: "100%",
+        height: "100%",
+        position: "relative",
+      }}
+    >
+      {blocks}
+    </div>,
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
 };
