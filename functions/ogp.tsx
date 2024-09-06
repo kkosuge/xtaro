64631728c9f71628d14c549e4c343c045abe8303
kkosuge <root@kksg.net>
2024-09-06 10:17:23 +0900

    noto sans bold

--- functions/ogp.tsx
+++ functions/ogp.tsx
@@ -1,12 +1,11 @@
 import { ImageResponse } from "@cloudflare/pages-plugin-vercel-og/api";
 
 export const onRequest: PagesFunction = async (context) => {
-  const url = context.request.url;
-  const search = new URL(url).searchParams;
+  const url = new URL(context.request.url);
+  const search = url.searchParams;
   const t = search.get("t") || "大きな文字でXに\nポストしてみませんか";
 
   const lines = t?.split("\n");
-
   const blocks = lines.map((line, index) => {
     let paddingTop = 630 / 2 - 64 * lines.length;
 
@@ -29,6 +28,10 @@ export const onRequest: PagesFunction = async (context) => {
     );
   });
 
+  const fontData = await fetch(
+    new URL(`${url.protocol}//${url.host}/fonts/NotoSansJP-ExtraBold.ttf`),
+  ).then((res) => res.arrayBuffer());
+
   return new ImageResponse(
     <div
       style={{
@@ -40,6 +43,7 @@ export const onRequest: PagesFunction = async (context) => {
         width: "100%",
         height: "100%",
         position: "relative",
+        fontFamily: "Noto Sans JP",
       }}
     >
       {blocks}
@@ -47,6 +51,14 @@ export const onRequest: PagesFunction = async (context) => {
     {
       width: 1200,
       height: 630,
+      emoji: "twimoji",
+      fonts: [
+        {
+          name: "Noto Sans JP",
+          data: fontData,
+          style: "bold",
+        },
+      ],
     },
   );
 };
